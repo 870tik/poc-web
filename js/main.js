1c243643adcd8d057bf338ff79ad3e0b4cc8c68f
花岡大樹 <>
2021-04-28 11:00:04 +0900

    修正

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-AOS.init();
+
+window.onload = function() {
+  AOS.init();
+}
+
+window.onscroll = function() {
+  var subContent = document.getElementById("sub");
+  console.log(subContent.getBoundingClientRect());
+  if(subContent.getBoundingClientRect().top < 300) {
+    subContent.style.transform = 'translate(0px' + subContent.getBoundingClientRect().top + ')'
+  }
+}
