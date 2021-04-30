efc08bde3fa2bde484f99ed2c380b29b2951fc1f
花岡大樹 <>
2021-04-30 20:38:02 +0900

    修正

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 var subContent = document.getElementById("sub");
+var subContentY;
 
 window.onload = function() {
   AOS.init();
-  subContent.style.transform = 'scale(0.8) translate(0, 20%)'
 }
 
 window.onscroll = function() {
   var py = subContent.getBoundingClientRect().top;
   var scroll = window.pageYOffset;
   var height = subContent.getBoundingClientRect().height;
-  if(py < height) {
-    subContent.style.transform = 'scale(1) translate(0, 0)'
+  if(py < height && ((height - py) / height) <= 1) {
+    subContent.style.width = (0.8 + ((height - py) / height)/10 * 2) * 100 + 'vw';
+    subContent.style.height = (0.8 + ((height - py) / height)/10 * 2) * 100 + 'vh';
+  } else if(py < height && ((height - py) / height) > 1) {
+    subContent.style.width = '100vw';
+    subContent.style.height = '100vh';
   }
 }
